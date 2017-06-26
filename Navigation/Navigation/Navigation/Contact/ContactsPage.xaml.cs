1364a9d934db1826a090da6a6d0b54bffde5bfe4
jinjingcai2014 <690649591@qq.com>
2017-06-26 23:27:05 +0800

    add Contact Page Navigation

---
+++ Navigation/Navigation/Navigation/Contact/ContactsPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navigation.Contact
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactsPage : ContentPage
+    {
+        public ContactsPage()
+        {
+            InitializeComponent();
+
+            listView.ItemsSource = new List<Contact>()
+            {
+                new Contact{Name ="Yu",ImageUrl="http://lorempixel.com/100/100/people/1"},
+                new Contact{Name="Caijingjin",ImageUrl="http://lorempixel.com/100/100/people/2",Status="Hey, let's talk"}
+            };
+        }
+
+        private async void Handle_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null) return;
+
+            var contact = e.SelectedItem as Contact;
+
+            await Navigation.PushAsync(new ContactDetailPage(contact));
+            listView.SelectedItem = null;
+        }
+    }
+}
